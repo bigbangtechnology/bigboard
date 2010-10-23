8d8b9df9add016fb9453e77e6aef32de7c63cef0
Cameron Westland <cameron@bigbangtechnology.com>
2010-10-23 15:42:58 -0400

    Added transitions support to the big board view

--- public/javascripts/views/big_board_view.js
+++ public/javascripts/views/big_board_view.js
@@ -1,29 +1,35 @@
 var BigBoardView = Backbone.View.extend(function() {
   
   //private
-  var currentState, taskStore, taskStoreView;   
+  var currentState, taskStore, taskStoreView;
+  
+  var transitions = {
+    "board_selected": {
+      "enter_state": function() {
+        taskStore = new TaskStore();
+        taskStoreView = new TaskStoreView({
+          model: taskStore
+        });
+
+        //add a fake task
+        taskStore.add([
+          { description: "A task" },
+          { description: "Another task" },
+          { description: "And yet another task" }
+        ]);
+      },
+    
+      "render_state": function() {
+        this.$('#taskStoreView').append(taskStoreView.el);
+      }          
+    }
+  }
   
   return {
     
     states: {
       NO_BOARD_SELECTED: "no_board_selected",
-      BOARD_SELECTED: "board_selected",
-      
-      transitions: {
-        "board_selected": function() {
-          taskStore = new TaskStore();
-          taskStoreView = new TaskStoreView({
-            model: taskStore
-          });
-
-          //add a fake task
-          taskStore.add([
-            { description: "A task" },
-            { description: "Another task" },
-            { description: "And yet another task" }
-          ]);
-        }
-      }
+      BOARD_SELECTED: "board_selected"
     },
   
     events: {
@@ -37,42 +43,48 @@ var BigBoardView = Backbone.View.extend(function() {
     render: function() {
       this.log("Rendering Big Board View");
       
-      this.updateState();
-  
-      $(this.el).html(this.template(this.model.toJSON()));
-      
-      // add the task store view
-      this.$('#taskStoreView').append(taskStoreView.el);
+      this.updateState(function(){
+        $(this.el).html(this.template(this.model.toJSON()));
+      });          
     
       this.handleEvents();
     
       return this;
     },
     
-    updateState: function() {	  
+    updateState: function(callback) {	  
       $(this.el).removeClass();
   
       var boardName = this.model.get('boardName');
       var state;
   
       if (boardName == undefined || boardName == null) {
-        this.transitionTo(this.states.NO_BOARD_SELECTED);
+        currentState = this.states.NO_BOARD_SELECTED;        
       } else {
-        this.transitionTo(this.states.BOARD_SELECTED);
+        currentState = this.states.BOARD_SELECTED;
       }
-  
-      $(this.el).addClass(currentState);
-    },
-  
-    /**
-      Transitions to the current state. If there are any transition callbacks defined
-      they will be called in the scope of the instance
-    **/
-    transitionTo: function(stateName) {
-      currentState = stateName;
       
-      if (this.states.transitions.hasOwnProperty(stateName)) {
-        this.states.transitions[stateName].call(this);
+      $(this.el).addClass(currentState);
+      this.executeTransition("enter_state");
+      
+      if (callback instanceof Function) {
+        callback.call(this);
+      }
+      
+      this.executeTransition("render_state");
+    },
+    
+    executeTransition: function(transitionName) {
+      if (this.hasTransition(transitionName)) {
+        transitions[currentState][transitionName].call(this);
+      }
+    },
+    
+    hasTransition: function(transitionName) {
+      if (transitions.hasOwnProperty(currentState) && transitions[currentState].hasOwnProperty(transitionName)) {
+        return true;
+      } else {
+        return false;
       }
     },
 
