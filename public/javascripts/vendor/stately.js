39ea2efae7649751ce4bca8db83c655a87d16c55
Cameron Westland <camwest@cam-macbook.local>
2010-10-24 16:34:44 -0400

    Updated stately

--- public/javascripts/vendor/stately.js
+++ public/javascripts/vendor/stately.js
@@ -13,12 +13,9 @@ var Stately = function() {
       
     },
 
-    /**
-      Revalidate state is a function that you call when you want to transition
-      into a new state potentially. It takes a single callback function as argument
-      which will execute once the state has been set but BEFORE the render_state function
-      is called for the given state.
-    **/    
+    // Revalidate state is a function that you call when you want to transition
+    // into a new state potentially. It takes a single callback function as argument
+    // which will execute once between the `before_transition` and `after_transition` functions
     revalidateState: function(callback) {
       if (this.currentState) {
         $(this.el).removeClass(this.currentState);
@@ -41,22 +38,18 @@ var Stately = function() {
       this.executeTransition("after_transition");
     },
 
-    /**
-      Executes a transition by name if it is defined in the View. 
-      Stately will automatically execute the enter_state and render_state
-      transitions if they are declared, custom transitions can be written
-      by declaring them and then running executeTransition manually
-    **/
+    // Executes a transition by name if it is defined in the View. 
+    // Stately will automatically execute the enter_state and render_state
+    // transitions if they are declared, custom transitions can be written
+    // by declaring them and then running executeTransition manually
     executeTransition: function(transitionName) {
       if (this.hasTransition(transitionName)) {
         this.transitions[this.currentState][transitionName].call(this);
       }
     },
 
-    /**
-      A helper method to check to see if the transition is defined
-      for the current state
-    **/
+    // A helper method to check to see if the transition is defined
+    // for the current state
     hasTransition: function(transitionName) {
       if (this.transitions.hasOwnProperty(this.currentState) && this.transitions[this.currentState].hasOwnProperty(transitionName)) {
         return true;
@@ -65,10 +58,8 @@ var Stately = function() {
       }
     },    
     
-    /**
-      Sub classes must override getState and return a string. getState is called
-      automatically when executing revalidateState
-    **/
+    // Sub classes must override getState and return a string. getState is called
+    // automatically when executing revalidateState
     getState: function() {
       // throws an error if this is not overwritten
       throw("Views must implement getState");
